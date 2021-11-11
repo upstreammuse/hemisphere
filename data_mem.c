fb173c32c10650ef467a481a9dc45bd577b77fc6
upstreammuse <upstreammuse@users.noreply.github.com>
2021-11-11 11:31:12 -0600

    Use bitset for page tracking, and make the page layout configurable

--- data_mem.c
+++ data_mem.c
@@ -3,54 +3,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "bitset.h"
 
 /* TODO the allocation scheme here is perhaps too complex and to no benefit, since data pages allocate and deallocate mostly randomly(?), but this scheme should be a good way to do stack pages, which may allocate and deallocate in batches(?) */
 
-/* 64KB data memory */
-uint8_t data_mem_array[256*256];
+#define PAGE_COUNT 256
+#define PAGE_COUNT_TYPE uint8_t
+#define PAGE_SIZE 256
+#define TASK_COUNT_TYPE uint16_t
+
+/* data page memory */
+uint8_t data_mem_array[PAGE_COUNT * PAGE_SIZE];
 
 /* bitmap that indicates allocated pages */
-/* TODO replace with bitset class to conserve memory */
-uint8_t used_pages[256];
+uint8_t used_pages_backing[PAGE_COUNT >> 3];
+BITSET* used_pages;
 
 /* efficiently indicate the next free page
  * when this is overflows back to 0, we look at the bitmap instead
  * on first use, the bitmap is also efficient, so we start cleanly
  */
-uint8_t next_free_page;
+PAGE_COUNT_TYPE next_free_page;
 
 struct page_table_entry {
-	uint16_t task;
-	uint8_t logical;
-	uint8_t physical;
+	TASK_COUNT_TYPE task;
+	PAGE_COUNT_TYPE logical;
+	PAGE_COUNT_TYPE physical;
 };
 
-struct page_table_entry page_table[256];
+/* TODO consider making the page table occupy some of the pages and be managed by an "OS" routine that hooks into the normal data read/write instructions, coupled with an "OS level" instruction to read/write raw memory addresses without checking */
+struct page_table_entry page_table[PAGE_COUNT];
 
 /* initialize data mem as at bootup */
 void init_data_mem() {
-	memset(used_pages, 0, 256);
+	used_pages = make_bitset(used_pages_backing, sizeof (used_pages_backing), PAGE_COUNT);
 	next_free_page = 0;
 }
 
-uint8_t allocate_page(uint16_t task, uint8_t logical) {
+PAGE_COUNT_TYPE allocate_page(TASK_COUNT_TYPE task, PAGE_COUNT_TYPE logical) {
 	/* if we are just starting, or if we have wrapped into bitmap mode, then find the next free page using the bitmap */
 	if (!next_free_page) {
-		for (int i = 0; i < 256; i++) {
-			if (!used_pages[i]) next_free_page = i;
-		}
+		next_free_page = find_clear_bit(used_pages, 0);
 	}
 
-	/* at this point, we are either in linear mode, and the bitmap should indicate a free page, or we found a free page above using the bitmap, so the bitmap should indicate a free page */
-	if (used_pages[next_free_page]) {
+	/* if we don't have a free page, we are broken */
+	/* TODO go into a debug halt state? */
+	if (get_bit(used_pages, next_free_page)) {
 		fprintf(stderr, "data pages exhausted\n");
-		/* TODO go into a debug halt state? */
 		exit(EXIT_FAILURE);
 	}
 
 	/* allocate the page into the page table */
-	used_pages[next_free_page] = 1;
-	for (int i = 0; i < 256; i++) {
+	/* TODO would like a way to assert that the page table had a free entry, but currently don't have that, and relying on deallocation logic to keep it in sync with allocated pages */
+	set_bit(used_pages, next_free_page);
+	for (size_t i = 0; i < PAGE_COUNT; i++) {
 		if (!page_table[i].task) {
 			page_table[i].task = task;
 			page_table[i].logical = logical;
@@ -58,11 +64,11 @@ uint8_t allocate_page(uint16_t task, uint8_t logical) {
 			break;
 		}
 	}
-	/* TODO would like a way to assert that the page table had a free entry, but currently don't have that.  maybe an allocation scheme like the pages themselves?  too heavy? */
 
 	/* if the next page isn't free, then we aren't in linear mode, so reset to bitmap mode */
+	/* if we just allocated the last page in linear mode, then we have entered bitmap mode */
 	next_free_page++;
-	if (used_pages[next_free_page]) {
+	if (get_bit(used_pages, next_free_page)) {
 		next_free_page = 0;
 	}
 }
